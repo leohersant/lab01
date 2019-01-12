e1181d341db8c36c70b8c0594988476d87f65518
leohersant <leohersant@gmail.com>
2019-01-12 11:32:47 -0500

    My first commit cmd line under linux

---
+++ README.md
@@ -0,0 +1,4 @@
+#My first lab from home
+
+Just another hello world, test.
+Goal: push code to git hub
---
+++ hello.c
@@ -0,0 +1,16 @@
+/* To compile hello. run the following command:
+   gcc -O3 -o hello hello.c
+
+  To run the program type:
+  ./hello
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+ // This is a dump comment
+ printf("Hello C un ubuntu\n");
+ return 0;
+}
